f59ee0f90eaff0670fd86c07ae58445671d70b75
eeredondo <E.REDONDO0314@GMAIL.COM>
2025-04-14 18:50:23 -0500

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,14 @@
+# backend/database.py
+
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./database.db"
+# Obtenemos la URL desde variables de entorno
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# Crear el motor (PostgreSQL no necesita connect_args)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
 Base = declarative_base()
