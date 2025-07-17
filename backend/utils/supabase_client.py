c0b07a5047869e606bbd05779df17d821e42a406
eeredondo <E.REDONDO0314@GMAIL.COM>
2025-07-17 08:06:56 -0500

    Update supabase_client.py

--- backend/utils/supabase_client.py
+++ backend/utils/supabase_client.py
@@ -1,10 +1,7 @@
-from supabase import create_client
+from supabase import create_client, Client
 import os
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-if not SUPABASE_URL or not SUPABASE_KEY:
-    raise RuntimeError("Supabase URL y/o Key no están definidos en variables de entorno")
-
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
